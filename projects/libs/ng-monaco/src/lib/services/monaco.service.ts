1553dd4d5c8cfab5d9405c328dd58bb69d0850cc
Benjamin Dobler <benz@richapps.de>
2024-12-04 14:24:52 +0100

    fix: monaco editor in require environment

--- projects/libs/ng-monaco/src/lib/services/monaco.service.ts
+++ projects/libs/ng-monaco/src/lib/services/monaco.service.ts
@@ -10,25 +10,51 @@ export class MonacoService {
   constructor() {
     this.load();
   }
+
   load() {
     if (!this.loadPromise) {
       this.loadPromise = new Promise((resolve, reject) => {
-        const script: HTMLScriptElement = document.createElement('script');
-        script.onerror = (e) => {
-          console.log('on error ', e);
+        var baseUrl = './assets/monaco/min/vs';
+
+        const REGQUIRE_CONFIG = {
+          preferScriptTags: true,
         };
 
-        script.onload = () => {
-          (window as any).require.config({ paths: { vs: '/assets/monaco/min/vs' } });
-          (window as any).require(['vs/editor/editor.main'], (monaco: MonacoAPI) => {
+        const onGotAmdLoader = (require: any) => {
+          let usedRequire = require || (window as any).require;
+          let requireConfig = { paths: { vs: `${baseUrl}` } };
+          Object.assign(requireConfig, REGQUIRE_CONFIG || {});
+          usedRequire.config(requireConfig);
+          usedRequire([`vs/editor/editor.main`], (monaco: MonacoAPI) => {
             resolve(monaco);
           });
         };
 
-        script.type = 'text/javascript';
-        script.src = '/assets/monaco/min/vs/loader.js';
-        script.async = true;
-        document.body.appendChild(script);
+        var src = `${baseUrl}/loader.js`;
+        var loaderRequest = new XMLHttpRequest();
+        loaderRequest.addEventListener('load', () => {
+          let scriptElem = document.createElement('script');
+          scriptElem.type = 'text/javascript';
+          if ('require' in window) {
+            scriptElem.text = `
+                            // Monaco uses a custom amd loader that over-rides node's require.
+                            // Keep a reference to node's require so we can restore it after executing the amd loader file.
+                            var nodeRequire = require;
+                            ${loaderRequest.responseText.replace('"use strict";', '')}
+                            // Save Monaco's amd require and restore Node's require
+                            var monacoAmdRequire = require;
+                            require = nodeRequire;
+                            require.nodeRequire = require;
+                        `;
+          } else {
+            scriptElem.text = loaderRequest.responseText.replace('"use strict";', '');
+          }
+
+          document.body.appendChild(scriptElem);
+          onGotAmdLoader((window as any).monacoAmdRequire);
+        });
+        loaderRequest.open('GET', src);
+        loaderRequest.send();
       });
     }
     return this.loadPromise;
